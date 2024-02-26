8325b3fe8599ac25644b25c30af4144628741357
unknown <imperia.zt@gmail.com>
2024-02-26 14:17:24 +0200

    add qantquantity

--- src/component/GoodsList.jsx
+++ src/component/GoodsList.jsx
@@ -1,15 +1,63 @@
 import GoodsCard from "./GoodsCard" 
+import React, {useState} from "react"
+
+
+
 
 
 
 function GoodsList (props){
 
 const {goods = [], addToCorzina = Function.prototype, closeAlert = Function.prototype} = props
+let [quantity, quantityState] = useState("16")
+
+console.log(quantity)
 
 
-    return   <div>
+const PlusQ = ()=>{
+    quantityState(++ quantity)
+}
+const MinusQ = ()=>{
+    quantityState(-- quantity)
+}
+
+
+const VaidQuantity = () => {
+    if (quantity >= goods.length){
+        return quantity = goods.length
+    } 
+    if (quantity <= 1){
+        return quantity = 1
+    } 
+}
+VaidQuantity()
+
+
+
+    return   <div className="boxcards">
+<div className="quantity">
+
+<p>введіть кількість позицій</p>
+<button 
+onClick={()=>MinusQ()}
+className="boxcards__btn">-</button>
+
+<input 
+    className="inputquantity"
+    id="input" 
+    type="text" 
+    value={quantity}
+    onChange={(e)=> quantityState(e.target.value)}
+></input>
+<button 
+onClick={()=>PlusQ()}
+className="boxcards__btn">+</button>   
+</div>
     <div className ="boxcard">
-    {goods.length ? goods.map(item => (<GoodsCard 
+    
+    {goods.length ? (
+    [...goods].splice(0, quantity)
+        ).map(item => (<GoodsCard 
     key={item.offerId}{...item} 
     addToCorzina={addToCorzina}
     closeAlert={closeAlert}
